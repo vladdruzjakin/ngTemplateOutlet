cdabb01f449465362bd8c51deb5782228d9f9a90
vladdruzjakin <drz.vladd@gmail.com>
2020-05-18 23:31:34 +0300

    ngTemplateOuylet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  public listImg = {
+    img: {
+      src: [
+        'https://lh3.googleusercontent.com/proxy/PAFOaFRnpE16mRH2kVL4s39lU4n_f7KVMVb7_qmon2tyNFf7aNWQJLZC_11lvFbw3krOnXfCZjpE98L9_KK_o1PoY3PhDQ',
+        'https://gdefon.org/_ph/4/2/236562616.jpg',
+        'https://anapet.info/wp-content/uploads/2010/08/super-nt-800.jpg',
+        'https://anapet.info/wp-content/uploads/2010/02/istok-800.jpg'
+      ]
+    }
+  };
 }
